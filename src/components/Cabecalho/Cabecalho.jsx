b876392c82899141f3a6ae1ba04d407ed64a4c9c
mthaugusto <mthaugusto94@icloud.com>
2023-10-04 09:17:17 -0300

    Estilizando o cabeçalho com saas, menu vertical

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
+import "./Cabecalho.scss"
 
 export default function Cabecalho(){
 
     return(
         <>
             <header className="cabecalho">
-                <h1>Vite + React</h1>
                 <ul>
                     <li><Link to="/">HOME</Link></li>
                     <li><Link to="/produtos">PRODUTOS</Link></li>
