17da2db686460c179c5ae59c5d5782b4674f5b9f
mthaugusto <mthaugusto94@icloud.com>
2023-10-04 08:59:42 -0300

    Refatorando o projeto e tirando itens do boyler-plate.
    Adicionando imagens estáticas.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,15 @@
 export default function Home(){
-
+document.title = "Home";
     return(
         <>
             <div>
-                <h1>HOME</h1>
+                <h1>Produtos em Oferta....</h1>
+                <figure>
+                    <figcaption>
+
+                    </figcaption>
+                    <img src="/online-shopping_640.jpg" alt="Produtos diversos" />
+                </figure>
             </div>
         </>
     )
